bb003c596101d1ede8c41aa5c387dabb05085706
ulugbek-ibrohimov <ulugbekdeveloper90@gmail.com>
2024-01-27 17:01:02 +0500

    add random

--- main.js
+++ main.js
@@ -6,19 +6,22 @@ var attemptCount = 6;
 var randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
 function findTheNumber(value) {
-  --attemptCount;
-  if (value <= 0 || isNaN(value)) {
-    elFindNumOutput.textContent = "0 katta son kiriting yokida raqam kiriting!";
-  } else if (value == randomNumber) {
+  // console.log(attemptCount);
+  if (value == randomNumber) {
     elFindNumOutput.textContent = "Siz yutdingiz 🙂";
     elFindNumInput.disabled = true;
-  } else if (attemptCount == 0) {
+  }
+  if (value > randomNumber) {
+    elFindNumOutput.textContent = "Siz kiritgan son random sondan katta!";
+    --attemptCount
+  } if (value < randomNumber) {
+    elFindNumOutput.textContent = "Siz kiritgan son random sondan kichik!";
+    --attemptCount
+  }
+  if (attemptCount <= 0) {
     elFindNumOutput.textContent = "Urinishlar soni tugadi!";
     elFindNumInput.disabled = true;
-  } else if (value > randomNumber) {
-    elFindNumOutput.textContent = "Siz kiritgan son random sondan katta!";
-  } else {
-    elFindNumOutput.textContent = "Siz kiritgan son random sondan kichik!";
+    attemptCount = 0;
   }
   elText.textContent = attemptCount;
 }
